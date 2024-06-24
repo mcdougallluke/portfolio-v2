2cc78e250ccc98c9c823d90405e29a61421b05db
mcdougallluke <mcdougallluke211@gmail.com>
2024-06-23 18:28:02 -0600

    Navbar fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useState, useEffect } from 'react';
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -6,13 +6,25 @@ import Skills from "./components/Skills";
 import Work from "./components/Work";
 
 function App() {
+  const [navOpen, setNavOpen] = useState(false);
+
+  useEffect(() => {
+    if (navOpen) {
+      document.body.classList.add('nav-open');
+    } else {
+      document.body.classList.remove('nav-open');
+    }
+  }, [navOpen]);
+
   return (
     <div>
-        <Navbar />
+      <Navbar navOpen={navOpen} setNavOpen={setNavOpen} />
+      <div className={`${navOpen ? 'pointer-events-none' : 'pointer-events-auto'}`}>
         <Home />
         <Skills />
         <Work />
         <Contact />
+      </div>
     </div>
   );
 }
